7caa5145a58946e0a951dbcee7d49a7299f71029
Entrodox <bindeeer@gmail.com>
2017-09-09 15:57:05 +0200

    megoldas

--- start.js
+++ start.js
@@ -2,47 +2,49 @@ var express = require("express");
 var app = express();
 var fs = require("fs");
 var request = require("request");
-
+var http = require("http");
 
 let configObj = JSON.parse(fs.readFileSync("config/config.json", "utf8"));
-let teamMembers = JSON.parse(fs.readFileSync("resources/team_members.json","utf8"));
+let teamMembers = JSON.parse(fs.readFileSync("resources/team_members.json", "utf8"));
 
-app.get("/team-members", function(req,res){
-    res.end(JSON.stringify(teamMembers));
+app.get("/team-members", function (req, res) {
+  res.end(JSON.stringify(teamMembers));
 });
 
-app.get("/competitors", function(req,res){
-  var teamInfos = [];
+app.get("/competitors", function (req, res) {
+  var teamInfos = [{"name": teamMembers.teamName}];
+  var chaosUrl = configObj.chaosBaseUrl + configObj.teamHash;
+  var counter = 0;
   request({
-      url: configObj.chaosBaseUrl + configObj.teamHash,
-      json: true
-    }, function(error,response,body){
-        if(!error && response.statusCode === 200){
-          var teamSize = body.length;
-          var counter = 0;
-            for(var i=0;i<teamSize;i++){
-              var currentUrl = body[i].url;
-              if(currentUrl.slice(-1) !== "/"){
-                currentUrl += "/";
-              }
-              if(currentUrl.substring(0,4) !== "http"){
-                currentUrl = "http://" + currentUrl;
-              }
-              currentUrl += "team-members";
-              request({
-                url: currentUrl
-              },function(error,response,body){
-                  if(!error && response.statusCode === 200){
-                    teamInfos.push(body);
-                    if(counter === teamSize-1){
-                      res.end(JSON.stringify(teamInfos));
-                    }
-                  }
-                  counter++;
-              })
-            }
+    url: chaosUrl,
+    json: true
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var urls = body;
+      for (i in urls) {
+        var url = urls[i].url;
+        if(url.indexOf("http") <= -1){
+          url = "http://" + url;
         }
-    });
+        url += url.slice(-1) == "/" ? "team-members" : "/team-members";
+        console.log(url);
+        request({
+          url: url,
+          json: true
+        }, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            if(!!body.teamName){
+              teamInfos.push({"name": body.teamName});
+            }
+          }
+          counter++;
+          if (counter == urls.length) {
+            res.end(JSON.stringify({"teams": teamInfos}));
+          }
+        })
+      }
+    }
+  })
 });
 
 
